8b549342c6bf963894f0f9d2b04798b73145ed33
stilSW <stilqn1975@gmail.com>
2023-04-20 16:28:20 +0300

    newCommit

---
+++ app.js
@@ -0,0 +1,54 @@
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground (event) {
+  const shapes = document.querySelectorAll(".shape")
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+  
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0 
+    const boolInt = isOdd ? -1 : 1 ;
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+  }
+}
+
+
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += " dark-theme";
+  } else {
+    document.body.classList.remove("dark-theme");
+  }
+}
+
+function contact(event) {
+  event.preventDefault();
+
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+
+  loading.classList += " modal__overlay--visible";
+  emailjs
+    .sendForm(
+      "service_ngljfuk",
+      "template_x2qyjin",
+      event.target,
+      "ho8q30oLV80HfmohC"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    });
+}
+
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
+}
